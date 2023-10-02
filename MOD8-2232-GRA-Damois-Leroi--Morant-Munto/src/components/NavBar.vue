fe9d46342e86b9bd9659405060792c6abdf1ced8
quentindamois <quentin.damois@efrei.net>
2023-10-01 20:59:23 -0400

    NavBar componment added for navigation added

---
+++ MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['user-input'])
+const UserInput = ref('')
+function sendUserInput() {
+    emit('user-input', UserInput.value)
+}
+</script>
+<template>
+    <nav>
+        <h3>
+            MTLSudentSpot
+        </h3>
+        <input type="text" v-model="UserInput" @input="sendUserInput">
+        <button>
+            Log In
+        </button>
+        <button>
+            Sign In
+        </button>
+    </nav>
+</template>
+<style>
+* {
+    background-color: aqua;
+}
+</style>
