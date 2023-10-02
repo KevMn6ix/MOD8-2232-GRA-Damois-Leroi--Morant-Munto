972ce3980062906d4af1ca8316c38bdb91f6760f
quentindamois <quentin.damois@efrei.net>
2023-10-02 02:03:09 -0400

    Prototype PlaceItem component added

---
+++ MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/components/PalceItem.vue
@@ -0,0 +1,35 @@
+<script>
+const placeSpecificity = defineProps({
+    rating: Number,
+    id: Number
+})
+</script>
+<template>
+    <RouterLink :to="'/places/' + placeSpecificity.id">
+    <slot name="picture">
+    </slot>
+    <slot name="title">
+    </slot>
+    <slot name="rating">
+
+    </slot>
+    <div v-if="placeSpecificity.rating=5">
+        *****
+    </div>
+    <div v-else-if="placeSpecificity.rating=4">
+        ****
+    </div>
+    <div v-else-if="placeSpecificity.rating=3">
+        ***
+    </div>
+    <div v-else-if="placeSpecificity.rating=2">
+        **
+    </div>
+    <div v-else-if="placeSpecificity.rating=1">
+        **
+    </div>
+    <div v-else>
+        0
+    </div>
+    </RouterLink> 
+</template>
