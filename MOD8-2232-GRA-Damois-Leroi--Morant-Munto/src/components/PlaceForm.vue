7b9f7be76f85dccc26664b1f07a726ed1c851fd0
quentindamois <quentin.damois@efrei.net>
2023-10-02 02:03:39 -0400

    Prototype PlaceForm component added

---
+++ MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/components/PlaceForm.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['response'])
+const placeInput = ref ({
+    Title: '',
+    Address: '',
+    PriceRating:0,
+    AmbianceRating:0,
+    HygieneRating:0
+})
+</script>
+<template>
+    <form method="post" onsubmit="return false">
+        <label>
+            Place's name
+        </label>
+        <input type="text" v-model="placeInput.Title">
+        <label>
+            Place's address
+        </label>
+        <input type="text" v-model="placeInput.Address">
+        <label>
+            Price Rating
+        </label>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <label>
+            Ambiance Rating
+        </label>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <label>
+            Hygiene Rating
+        </label>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            *
+        </button>
+        <button>
+            Post Rating
+        </button>
+    </form>
+</template>
