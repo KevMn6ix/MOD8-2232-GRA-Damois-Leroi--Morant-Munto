7a2d2510d755bd6e139266cb7404bc0a7c9c4d17
quentindamois <quentin.damois@efrei.net>
2023-10-01 21:51:08 -0400

    ButtonNavBar componment added

---
+++ MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/components/ButtonNavBar.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['user-input'])
+function RestaurantPressed() {
+    emit('user-input', 'restaurant');
+    document.getElementById('restaurant').setAttribute("class", "pressed");
+    document.getElementById('travel').setAttribute("class", "unpressed");
+    document.getElementById('activity').setAttribute("class", "unpressed");
+}
+function TravelPressed() {
+    emit('user-input', 'travel');
+    document.getElementById('travel').setAttribute("class", "pressed");
+    document.getElementById('restaurant').setAttribute("class", "unpressed");
+    document.getElementById('activity').setAttribute("class", "unpressed");
+}
+function ActivityPressed() {
+    emit('user-input', 'activity');
+    document.getElementById('activity').setAttribute("class", "pressed");
+    document.getElementById('travel').setAttribute("class", "unpressed");
+    document.getElementById('restaurant').setAttribute("class", "unpressed");
+}
+</script>
+<template>
+    <nav>
+        <button id="restaurant" class="unpressed" @click="RestaurantPressed">Restaurant</button>
+        <button id="travel" class="unpressed" @click="TravelPressed">Travel</button>
+        <button id="activity" class="unpressed" @click="ActivityPressed">Activities</button>
+    </nav>
+</template>
+<style>
+root {
+    --light-color-background: #d9d9d9;
+    --light-color-backgrounud-selected: #a9a9a9;
+    --light-color-text: black;
+    --dark-color-background: #272727;
+    --dark-color-background-selected: #575757;
+    --dark-color-text: white;
+    --color-background: var(--light-color-background);
+    --color-background-selected: var(--light-color-backgrounud-selected);
+    --color-text: var(--light-color-text);
+    @media (prefers-color-scheme: dark) {
+        --color-background: var(--dark-color-background);
+        --color-background-selected: var(--dark-color-backgrounud-selected);
+        --color-text: var(--dark-color-text);
+    }
+}
+#unpressed {
+    color: var(--color-text);
+    background-color: var(--color-background);
+}
+#unpressed {
+    color: var(--color-text);
+    background-color: var(--color-background-selected);
+}
+</style>
