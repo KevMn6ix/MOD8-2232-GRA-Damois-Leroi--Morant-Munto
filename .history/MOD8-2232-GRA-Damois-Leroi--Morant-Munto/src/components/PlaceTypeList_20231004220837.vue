998a1a8b50aea16c06f81828f5da358b7912e724
olivier.9.1 <olivier.leroimorant@gmail.com>
2023-10-05 09:58:14 -0400

    Cleared up Problems

---
+++ .history/MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/components/PlaceTypeList_20231004220837.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import PlaceItem from './PlaceItem.vue';
+    import { ref, computed, routes } from 'vue';
+
+
+    const propsList = defineProps({
+        placetype: String,
+        placeArray: Object
+    })
+
+    computed : {
+
+        
+
+    }
+
+    const filteredPlaces = ref([]);
+</script>
+
+<template>
+
+    <ul>
+
+        <li v-for="place in propsList.placeArray.filer((p) => {} )">
+        </li>
+    </ul>
+
+</template>
