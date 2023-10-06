09369758160158c0d0227efe52ac30d02c320036
quentindamois <quentin.damois@efrei.net>
2023-10-06 15:36:05 -0400

    created a functionnal part of the page to add a new place

--- MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/views/AddPageView.vue
+++ MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/views/AddPageView.vue
@@ -1,7 +1,41 @@
-<script setup>
+<script>
+import { RouterLink } from 'vue-router';
 import TheAddPlaceForm from '../components/TheAddPlaceForm.vue';
+import BarOutsideHome from '../components/BarOutsideHome.vue';
+export default{
+    name: 'RegisterForm',
+    data() {
+        return {
+            lastName: '',
+            firstName: '',
+            email: '',
+            phoneNumber: '',
+            password: '',
+            passwordConfirm: ''
+        };
+    },
+    methods: {
+        handleSubmit() {
+            const userData = {
+                lastName: this.lastName,
+                firstName: this.firstName,
+                email: this.email,
+                phoneNumber: this.phoneNumber,
+                password: this.password,
+                passwordConfirm: this.passwordConfirm
+            };
+            console.log(userData);
+            console.log("Registered");
+        }
+    },
+    components: { RouterLink, BarOutsideHome }
+}
+
 </script>
 <template>
+    <header>
+        <BarOutsideHome/>
+    </header>
     <h1>
         Reference any place you like to hang out
     </h1>
