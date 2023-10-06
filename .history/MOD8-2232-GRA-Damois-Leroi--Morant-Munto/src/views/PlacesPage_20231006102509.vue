120076168290dd5cdc9e05c0c0234aa2f240d646
olivier.9.1 <olivier.leroimorant@gmail.com>
2023-10-06 10:45:12 -0400

    Added The Place Page

---
+++ .history/MOD8-2232-GRA-Damois-Leroi--Morant-Munto/src/views/PlacesPage_20231006102509.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+    import { ref, defineProps } from 'vue';
+    
+    const props = defineProps({
+        placeList: Array, // Update the type to match the actual data type of placeList
+    });
+</script>
+
+<template>
+    
+
+
+</template>
