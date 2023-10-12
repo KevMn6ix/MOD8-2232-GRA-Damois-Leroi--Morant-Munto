990705768ecc53213f3b9d7cbc7365b08daaf0fe
quentindamois <quentin.damois@efrei.net>
2023-10-11 23:35:48 -0400

    add the template file

---
+++ template/template-web/client/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import PageHeader from './components/PageHeader.vue'
+import PageFooter from './components/PageFooter.vue'
+</script>
+
+<template>
+  <PageHeader />
+  <RouterView />
+  <PageFooter />
+</template>
+
+<style>
+#app > header,
+#app > main,
+#app > footer {
+  padding: var(--padding-large);
+}
+
+#app > main {
+  flex-grow: 1;
+}
+
+.content {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: var(--padding-large);
+  max-width: var(--max-width);
+  margin: 0 auto;
+}
+
+.app-link * {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: var(--color-heading);
+
+  & a:link,
+  a:visited {
+    color: var(--color-heading);
+  }
+}
+
+@media (min-width: 768px) {
+  form {
+    max-width: 512px;
+    margin: 0 auto;
+  }
+}
+</style>
---
+++ template/template-web/client/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    }
+  ],
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+export default router
