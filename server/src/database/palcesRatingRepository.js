3e33eb17a5868f42ea007ee1e72860a185f52c3c
quentindamois <quentin.damois@efrei.net>
2023-10-16 15:18:42 -0400

    script to addd review and get one added

---
+++ server/src/database/palcesRatingRepository.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+var connection = require('./database')
+
+
+const app = express()
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+module.exports = {
+    findRating (req, res) {
+        const idPlace = req.body.idPlace;
+
+        var sql = "SELECT * FROM rating WHERE idPlace = ?"
+        connection.query(sql, [idPlace],
+            (err, result) => {
+                console.log(err);
+            }
+        )
+        res.send(res.result);
+    },
+    addRating (req, res) {
+        const id = req.body.id;
+        const IdPlace = req.body.IdPlace;
+        const hygieneRating = req.body.hygieneRating;
+        const ambianceRating = req.body.ambianceRating;
+        const priceRating = req.body.priceRating;
+        const rating = req.body.rating;
+        const commentary = req.body.commentary;
+
+        var sql = "INSERT INTO rating (id, place_id, rating, hygiene_rating, ambiance_rating, price_rating, commentary) VALUES (?,?,?,?,?,?,?)"
+        connection.query(sql, [id, IdPlace, rating, hygieneRating,  ambianceRating, priceRating, commentary],
+            (err, result) => {
+                console.log(err);
+            }
+        )
+        res.send({
+            message : `Thank you, ${req.body.id} have been successfully added !`
+        })
+    },
+}
